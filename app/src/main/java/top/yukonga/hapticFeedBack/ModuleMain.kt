7e06ad0587b3d8c1cbd799bcd2cd0f6fd5b666ea
Xuanzheng0901 <szx1691196289@gmail.com>
2026-03-21 15:46:23 +0800

    fix: 直接修改版本会导致一些v2特性消失, 故改为hook更高层判断是否触发Feedback逻辑

--- app/src/main/java/top/yukonga/hapticFeedBack/ModuleMain.kt
+++ app/src/main/java/top/yukonga/hapticFeedBack/ModuleMain.kt
@@ -20,21 +20,9 @@ class ModuleMain(base: XposedInterface, param: XposedModuleInterface.ModuleLoade
         super.onPackageLoaded(param)
         loadedPackageParam = param
         if (param.packageName == "com.miui.home") {
-            //Hook HapticFeedbackCompat.doesSupportHapticV2 to always return false
-
-            val hapticFeedbackCompat = "com.miui.home.common.hapticfeedback.HapticFeedbackCompat"
-            val hapticFeedbackCompatClass = param.classLoader.loadClass(hapticFeedbackCompat)
-
-                try {
-                    val field = hapticFeedbackCompatClass.getDeclaredField("sIsSupportHapticV2")
-                    field.isAccessible = true
-                    field.setBoolean(null, false)
-                    if (BuildConfig.DEBUG) module.log("set sIsSupportHapticV2 = false via reflection")
-                } catch (e: Throwable) {
-                    if (BuildConfig.DEBUG) module.log("failed to set sIsSupportHapticV2: ${e.message}")
-                }
-            hookMethods(hapticFeedbackCompatClass, DoesSupportHapticV2Hook::class.java, "doesSupportHapticV2")
-        }
+            val gesturesBackClass = param.classLoader.loadClass("com.miui.home.recents.GesturesBackTouchProcessor")
+            //强制执行Feedback
+            hookMethods(gesturesBackClass, IsDoFeedBackHook::class.java, "isDoFeedBack")}
     }
 
     private fun hookMethods(clazz: Class<*>, hooker: Class<out XposedInterface.Hooker?>, vararg names: String) {
@@ -45,12 +33,13 @@ class ModuleMain(base: XposedInterface, param: XposedModuleInterface.ModuleLoade
     }
 }
 
-class DoesSupportHapticV2Hook : XposedInterface.Hooker {
+
+class IsDoFeedBackHook : XposedInterface.Hooker {
     companion object {
         @JvmStatic
         fun before(callback: XposedInterface.BeforeHookCallback) {
-            if (BuildConfig.DEBUG) module.log("hooking doesSupportHapticV2 -> true")
-            // Force the method to return false and skip original implementation
+            if (BuildConfig.DEBUG) module.log("hooking isDoFeedBack -> true")
+            // 强制返回 true 并跳过原方法
             callback.returnAndSkip(true)
         }
     }
